8ec2e20d61e9ac0a2a3cf16838c2f7a41ce5500b
Kamila <tashimovakamila1@gmail.com>
2024-05-16 14:42:24 +0500

    rm obj_dir

--- BittyPro/obj_dir/Vmain__ver.d
+++
@@ -1,1 +0,0 @@
-obj_dir/Vmain.cpp obj_dir/Vmain.h obj_dir/Vmain.mk obj_dir/Vmain__Slow.cpp obj_dir/Vmain__Syms.cpp obj_dir/Vmain__Syms.h obj_dir/Vmain__Trace.cpp obj_dir/Vmain__Trace__Slow.cpp obj_dir/Vmain__ver.d obj_dir/Vmain_classes.mk  : /usr/bin/verilator_bin /usr/bin/verilator_bin Reg.v alu.v controlunit.v main.v mux.v 
--- BittyPro/obj_dir/tb_main.d
+++
@@ -1,5 +0,0 @@
-tb_main.o: ../tb_main.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vmain.h
